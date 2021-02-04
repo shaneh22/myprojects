ccd5ae24363d9952c2d3e22e3f85cf696031aa22
shaneh22 <shaneh22@live.unc.edu>
2021-02-03 23:06:43 -0500

    Micro Metroidvania FINISHED

---
+++ UnityProjects/Micro Metroidvania/Assets/Scripts/Disappear.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disappear : MonoBehaviour
+{
+    public void Collision()
+    {
+        StopAllCoroutines();
+        gameObject.SetActive(false);
+        Invoke(nameof(Reset), 3f);
+    }
+    private void Reset()
+    {
+        gameObject.SetActive(true);
+    }
+}
